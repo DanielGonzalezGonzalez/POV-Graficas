5631cd62ca592a33a76cd40d52d41c4c3a355f46
DanielGonzalezGonzalez <dano.gonzalez0@gmail.com>
2018-03-22 15:25:50 -0600

    ~

--- Julia.py
+++ Julia.py
@@ -3,28 +3,44 @@ import random
 
 if __name__ == "__main__":
 
-    w, h, zoom = 640,480,1
+    '''
+    Canvas size
+    '''
+    w, h = 500,500
 
     bitmap = Image.new("RGB", (w, h), "white")
-
     pix = bitmap.load()
 
-
-    cX, cY = -0.7, 0.27015
-    moveX, moveY = 0.0, 0.0
+    '''
+    cx, cy = equation for the fractal
+    maxIter = maximum number of iterations
+    '''
+    cX, cY = 0.285, 0.01
     maxIter = 199
-    print(maxIter)
 
     for x in range(w):
         for y in range(h):
-            zx = 1.5*(x - w/2)/(0.5*zoom*w) + moveX
-            zy = 1.0*(y - h/2)/(0.5*zoom*h) + moveY
+            '''
+            zx, zy = Scaled coordinates
+            '''
+            zx = 1.5*(x - w/2)/(0.5*w)
+            zy = 1.0*(y - h/2)/(0.5*h)
             i = maxIter
+            '''
+            Break when iterations droped to 1 or the scaled coordinates
+            achieve the size of the imaginary plane
+            '''
             while zx*zx + zy*zy < 4 and i > 1:
                 tmp = zx*zx - zy*zy + cX
                 zy,zx = 2.0*zx*zy + cY, tmp
                 i -= 1
 
+            '''
+            Assign a color to the specific pixel.
+            '''
             pix[x,y] = (i << 21) + (i << 10) + i*8
 
+    '''
+    Print the pixels.
+    '''
     bitmap.show()
